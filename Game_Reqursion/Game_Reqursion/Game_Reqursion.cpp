c3cd883039fd1f1bd9fb93dded4bbc7d65a4f62f
gregshevchenko3 <gregshevchenko3@gmail.com>
2020-02-18 08:59:46 +0200

    Реалізація гри "Хрестики-Нолики", людина проти компа.

--- Game_Reqursion/Game_Reqursion/Game_Reqursion.cpp
+++ Game_Reqursion/Game_Reqursion/Game_Reqursion.cpp
@@ -1,63 +1,180 @@
 #include <iostream>
+#include <algorithm>
 #include <conio.h>
 
-char field[3][3] = {
-	{ ' ', ' ', ' ' },
-	{ ' ', ' ', ' ' },
-	{ ' ', ' ', ' ' }
+// Ігрове поле
+char field[3][3] = { 
+	{' ', ' ', ' ',},
+	{' ', ' ', ' ',},
+	{' ', ' ', ' ',},
 };
 
-bool equ_vert(int vert_num, char c) {
-	bool result = true;
-	for (auto i = 0; i < 3; i++)
-		result = result && (field[i][vert_num] == c);
-	return result;
-}
-bool equ_horiz(int horiz_num, char c) {
-	bool result = true;
-	for (auto i = 0; i < 3; i++)
-		result = result && (field[horiz_num][i] == c);
-	return result;
-}
-bool equ_diag(char ch) {
-	bool result = true, res2 = true;
-	for (auto r = 0, c = 0; c < 3 && r < 3; c++, r++)
-		result = result && (field[r][c] == ch);
-
-	for (auto r = 0, c = 2; c >= 0 && r < 3; c--, r++)
-		res2 = res2 && (field[r][c] == ch);
-
-	return result || res2;
-}
-
-bool Player_Win(char ch) {
-	bool result = false;
-	for (int i = 0; i < 3; i++) {
-		result = result || equ_vert(i, ch) || equ_horiz(i, ch);
-	}
-	return result || equ_diag(ch);
-}
-
-char* get_field(int NumLockKey) {
-	switch (NumLockKey) {
-	case 49: return &field[2][0];
-	case 50: return &field[2][1];
-	case 51: return &field[2][2];
-	case 52: return &field[1][0];
-	case 53: return &field[1][1];
-	case 54: return &field[1][2];
-	case 55: return &field[0][0];
-	case 56: return &field[0][1];
-	case 57: return &field[0][2];
-	default:
-		return 0;
-	}
-}
-
+// Комп'ютерний гравець.
+void calc_comp_move();
+// Перевірка ігрового поля;
 void check(bool, int);
+// Хід 
 void move(bool, int);
+// Отрисовка поля
+void update(bool player, int move_count);
+/**
+ * Рахує рейтинг по рядках, стовцях, та діагонялях. 
+ * Результат записує в масив, що переданий в якості аргументу.
+ * 
+ * Виикористовується при перевірці ігрового поля та при прорахунку ходу ком'пютером.
+ */
+void create_rating_vector(int atoms[8]) {
+	for (auto i = 0; i < 8; i++) atoms[i] = 0;
+	for (auto i = 0; i < 3; i++) {
+		for (auto j = 0; j < 3; j++) {
+			// сума рядка
+			if (field[i][j] == 'X') atoms[i] += 1;
+			if (field[i][j] == 'O') atoms[i] -= 1;
+			// сума колонки
+			if (field[j][i] == 'X') atoms[i + 3] += 1;
+			if (field[j][i] == 'O') atoms[i + 3] -= 1;
+		}
+		// сума по основній діагоналі
+		if (field[i][i] == 'X') atoms[6] += 1;
+		if (field[i][i] == 'O') atoms[6] -= 1;
+		// сума по побічній діагоналі
+		if (field[i][2 - i] == 'X') atoms[7] += 1;
+		if (field[i][2 - i] == 'O') atoms[7] -= 1;
+	}
+}
+/**
+ * Виріщує куди вписувать "O". результат передає в змінні, передані за посиланнями res_row, res_col;
+ * koef_idx - фактор, за яким приймається рішення, це позиція елемента в масиві рейтигів, обчисленому
+ * функцією create_rating_vector().
+ */
+void solve(int koef_idx, int& res_row, int& res_col) {
+	if (koef_idx < 3) {
+		res_row = koef_idx;
+		res_col = std::find(field[koef_idx], field[koef_idx] + 3, ' ') - field[koef_idx];
+	}
+	else if (koef_idx < 6) {
+		res_col = koef_idx;
+		char ar[3] = { field[0][koef_idx], field[1][koef_idx], field[2][koef_idx] };
+		res_row = std::find(ar, ar + 3, ' ') - ar;
+	}
+	else if (koef_idx == 6) {
+		char ar[3] = { field[0][0], field[1][1], field[2][2] };
+		int i = std::find(ar, ar + 3, ' ') - ar;
+		res_col = res_row = i;
+	}
+	else {
+		char ar[3] = { field[0][2], field[1][1], field[2][0] };
+		int i = std::find(ar, ar + 3, ' ') - ar;
+		res_row = i, res_col = 2 - i;
+	}
+}
+/**
+ * Власне логіка компа. Без передбачень. Можливості навчання і т.д.
+ * Прото тупий кінцевий автомат. Деталі по тексту функції
+ */
+void calc_comp_move() {
+	int atoms[8];
+	create_rating_vector(atoms);
 
-void update(bool start, int move_count) {
+	int row = 0, col = 0;
+	auto mx = std::max_element(atoms, atoms + 8), mn = std::min_element(atoms, atoms + 8);
+	int mx_idx = mx - atoms, mn_idx = mn - atoms;
+	/*
+	 * Якщо сума елементів в ряду, в стовпці, або в діагональ матриці дорівнює -2, то а цим ходом можна виграти,
+	 * вписавши у вільну клітинку ряду, стовпця, діагоналі відповідно "O"
+	 */
+	if (*mn == -2)
+		solve(mn_idx, row, col);
+	else
+		/*
+		 * Якщо сума елементів в ряду, в стовпці, або в матриці дорівнює 2, то аби не програти - вписуємо
+		 * у вільну клітинку ряду, стовпця, діагоналі відповідно "O"
+		 */
+		if (*mx == 2)
+			solve(mx_idx, row, col);
+		else
+			/*
+			 * Якщо ні виграш, ні програш не грозять, записуємо "O" у вільну клітинку ряду, стовпця, діагоналі відповідно, якщо сума елементів - найменша.
+			 */
+			solve(mn_idx, row, col);
+	field[row][col] = 'O';
+}
+// Що натиснув гравець?
+char* get_field() {
+	int key;
+	do {
+		key = _getch();
+		if (key == 27) {
+			return 0;
+		};
+	} while (key < 49 || key > 57);
+	int row = 2 - ((key - 49) / 3), col = (key - 49) % 3;
+	return &field[row][col];
+}
+
+
+void check(bool player, int move_count) {
+	int atoms[8];
+	create_rating_vector(atoms);
+
+	if (3 == *std::max_element(atoms, atoms+8)) {
+		std::cout << "Ви виграли!!!" << std::endl;
+		return;
+	}
+	if (-3 == *std::min_element(atoms, atoms+8)) {
+		std::cout << "Ви програли!!!" << std::endl;
+		return;
+	}
+	
+	if (move_count == 0){
+		std::cout << "Нічия..." << std::endl;
+		return;
+	}
+	// гравець натиснув Esc (Здався)
+	if (move_count == -1) {
+		if (player) {
+			std::cout << "Ви здалися!!!" << std::endl;
+			return;
+		}
+	}
+	move(player, move_count);
+}
+/**
+ * Хід.
+ * player == true - грає живий гравець
+ * player == false - грає комп'ютер.
+ *
+ * move_count > 0 - гра продовжується. Через цей параметр відслідковується нічия, та технічна поразка (гравець здався)
+ */
+void move(bool player, int move_cont) {
+	if (player) {
+		char* fld = 0;
+		do {
+			fld = get_field();
+			if (!fld) {
+				check(player, -1);
+				return;
+			}
+		} while (*fld != ' ');
+		if (*fld != 'X' && *fld != 'O') {
+			if (player) *fld = 'X';
+			else
+				*fld = 'O';
+		}
+	}
+	else {
+		calc_comp_move();
+	}
+	update(!player, move_cont - 1);
+}
+/**
+ * Малюює поле.
+ * player == true - грає живий гравець
+ * player == false - грає комп'ютер.
+ *
+ * move_count > 0 - гра продовжується. Через цей параметр відслідковується нічия, та технічна поразка (гравець здався)
+ */
+void update(bool player, int move_count) {
 	system("cls");
 	for (int i = 0; i < 3; i++) {
 		if (i == 0)
@@ -68,65 +185,18 @@ void update(bool start, int move_count) {
 		std::cout << "+---+---+---+" << std::endl;
 	}
 	std::cout << std::endl;
-	if (start)
-		std::cout << "Хід гравця №1:" << std::endl;
-	else
-		std::cout << "Хід гравця №2:" << std::endl;
-	check(start, move_count);
-}
-
-void move(bool player, int move_cont) {
-	int key;
-	do {
-		key = _getch();
-		if (key == 27) {
-			check(player, -1);
-		};
-	}
-	while (key < 49 || key > 57);
-
-	char* fld = get_field(key);
+	if (player)
+		std::cout << "Ваш хід" << std::endl;
 	
-	if (*fld != 'X' && *fld != 'O') {
-		if (player) *fld = 'X';
-		else
-			*fld = 'O';
-	}
-	update(!player, move_cont - 1);
-	
+	check(player, move_count);
 }
 
-void check(bool start, int move_count) {
-	if (Player_Win('X')) {
-		std::cout << "Гравець №1 - виграв!!!" << std::endl;
-		return;
-	}
-	if (Player_Win('O')) {
-		std::cout << "Гравець №2 - виграв!!!" << std::endl;
-		return;
-	}
-	if (move_count == 0) {
-		std::cout << "Нічия..." << std::endl;
-		return;
-	}
-	if (move_count == -1) {
-		if (start) {
-			std::cout << "Гравець №1 - здався!!!" << std::endl;
-			std::cout << "Гравець №2 - виграв!!!" << std::endl;
-		}
-		else {
-			std::cout << "Гравець №2 - здався!!!" << std::endl;
-			std::cout << "Гравець №1 - виграв!!!" << std::endl;
-		}
-	}
-	move(start, move_count);
-}
 #include "windows.h"
 int main()
 {
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
 	setlocale(LC_ALL, "Ukranian");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
 
 	update(true, 9);
 	std::cout << "GAME OVER!" << std::endl;
