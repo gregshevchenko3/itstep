bbd05908d3a9e38b21c66ed749dc125d3ef573d0
gregshevchenko3 <gregshevchenko3@gmail.com>
2020-04-28 14:04:03 +0300

    + Підтримка дійсних чисел. Ділення на 0 обробляється тільки для цілих чисел. Остача вичисляється при діленні цілих чисел. Степінь вичисляється за допомогою std::pow() результат додатково не обробляється. Тип результату того чи іншого оператора залежить від типу операндів.
    + Добавлено помилку "не число"
    + Добавлено помилку "Операція не пдтримується"

--- calc/calc/Source1.cpp
+++ calc/calc/Source1.cpp
@@ -1,7 +1,14 @@
-//#ifdef version2
 #include <iostream>
 #include <cstring>
+#include <cerrno>
 #include <cmath> // for std::pow() in linux
+
+
+union numeric {
+	double d;
+	long long ll;
+};
+
 
 char* get_line();
 /**
@@ -20,14 +27,13 @@ char* get_line();
  *
  * prev_operator_priority - При викликові з програми - не має сенсу, тому завжди 0. В рекурсії - пріоритет попереднього оператора.
  */
-long long parse(char** expr, long *error_code, long long* lhs = nullptr, bool unary = true, char prev_operator_priority = 0);
+numeric parse(char** expr, bool& is_integral_result, long &error_code, numeric* lhs = nullptr, bool is_int_lhs = true, bool unary = true, char prev_operator_priority = 0);
 /**
  * Об'єднує в собі strtoll() та strtod(). Якщо не виходить повністю інтерпретувати str, як число - повертає false.
  * якщо вдається інтерпретувати str, як ціле число, повертає true, is_integer встановлює в true, а число в ll_num,
  * якщо вдається інтерпретувати str, як дійсне число, повертає true, is_integer встановлює в false, а число в d_num.
  */
-bool strtonum(char* str, long& is_integer, long long& ll_num, double& d_num);
-//double parse(char** expr, double* lhs = nullptr, bool unary = true, char prev_operator_priority = 0);
+bool strtonum(char* str, bool& is_integer, numeric& num);
 
 // Повідомлення про помилку.
 const char* const error_msg[] = {
@@ -56,10 +62,6 @@ const char* const postfix_tokens = "!)"; // Тільки постфіксні
 const char* const priority =  "112223000";
 const char* const upriority = "0400006-5";
 
-
-
-
-
 int main() //for linux`s g++
 {
 	std::cout << "Simple console calc. For exit - press \"Ctrl+C\"" << std::endl;
@@ -67,14 +69,14 @@ int main() //for linux`s g++
 		std::cout << "calc> ";
 		char* line = get_line();
 		char* tmp = line;
-
+		bool is_result_integral = true;
 		long err_code = 0;
-		long long result = parse(&tmp, &err_code, (long long*)nullptr);
+		numeric result = parse(&tmp, is_result_integral, err_code);
 		if (err_code > 1) {
 			std::cout << "Error: " << error_msg[err_code] << std::endl;
 		}
 		else if(err_code != 1){
-			std::cout << result << std::endl;
+			std::cout << ((is_result_integral)?result.ll:result.d )<< std::endl;
 		}
 		tmp = nullptr;
 		delete[] line;
@@ -97,11 +99,13 @@ char* get_line()
 	line[count - 1] = 0;
 	return line;
 }
-long long parse(char** expr, long *error_code, long long* lhs, bool unary, char prev_operator_priority)
+numeric parse(char** expr, bool& is_integral_result, long &error_code, numeric* lhs, bool is_int_lhs, bool unary, char prev_operator_priority)
 {
 	if (**expr == 0) {
-		*error_code = 1;
-		return 0;
+		error_code = 1;
+		numeric ll;
+		ll.ll = 0;
+		return ll;
 	}
 	char* tmp = new char[strlen(*expr) + 1];
 	strcpy(tmp, *expr);
@@ -131,20 +135,29 @@ long long parse(char** expr, long *error_code, long long* lhs, bool unary, char 
 		is_unary_next_operator = !strchr(postfix_tokens, this_operator) && (it - tmp > 1);
 		is_unary_next_operator = is_unary_next_operator || strchr(postfix_tokens, *_next_operator) && (strchr(tokens, *(_next_operator + 1)) || *(_next_operator + 1) == 0);
 	}
-
 	// пріоритет цього та наступного операторів.
 	char this_operator_priority = 0, next_operator_priority = 0;
 	if (*_next_operator) {
 		this_operator_priority = *((unary) ? (upriority + (strchr(tokens, this_operator) - tokens)) : (priority + (strchr(tokens, this_operator) - tokens)));
 		next_operator_priority = *((is_unary_next_operator) ? (upriority + (strchr(tokens, *_next_operator) - tokens)) : (priority + (strchr(tokens, *_next_operator) - tokens)));
 	}
-	long long expr_rhs = 0, _rhs = 0;
-	if(it)
-		_rhs = strtoll(it, nullptr, 0);
+	
+	numeric expr_rhs, _rhs; 
+	bool is_int_rhs = true;
+	expr_rhs.ll = 0;
+	_rhs.ll = 0;
+	if (it) {
+		if (!strtonum(it, is_int_rhs, _rhs)) {
+			error_code = 7; // NaN
+			_rhs.ll = 0;
+			return _rhs;
+		}
+	}
 	else 
 		if (!unary) {
-			*error_code = 3; // Unknown binary operator;
-			return 0;
+			error_code = 3; // Unknown binary operator;
+			_rhs.ll = 0;
+			return _rhs;
 		}
 	delete[] tmp;
 	/**
@@ -154,7 +167,7 @@ long long parse(char** expr, long *error_code, long long* lhs, bool unary, char 
 	if (next_operator_priority != '-' && next_operator_priority > this_operator_priority && this_operator_priority != '-') {
 		expr_rhs = _rhs;
 		while (next_operator_priority > this_operator_priority && this_operator) {
-			_rhs = parse(&_next_operator, error_code, &expr_rhs, is_unary_next_operator, this_operator_priority);
+			_rhs = parse(&_next_operator, is_int_rhs, error_code, &expr_rhs, is_int_rhs, is_unary_next_operator, this_operator_priority);
 			expr_rhs = _rhs;
 			is_unary_next_operator = strchr(postfix_tokens, *_next_operator) && (strchr(tokens, *(_next_operator + 1)) || *(_next_operator + 1) == 0);
 			next_operator_priority = *((is_unary_next_operator) ? (upriority + (strchr(tokens, *_next_operator) - tokens)) : (priority + (strchr(tokens, *_next_operator) - tokens)));
@@ -172,7 +185,10 @@ long long parse(char** expr, long *error_code, long long* lhs, bool unary, char 
 			break;
 		case '-':
 			// Унарний мінус
-			_rhs = -expr_rhs;
+			if(is_int_rhs)
+				_rhs.ll = -expr_rhs.ll;
+			else
+				_rhs.d = -expr_rhs.d;
 			break;
 		/*case '!':
 			// Факторіал лівої частини.
@@ -181,56 +197,101 @@ long long parse(char** expr, long *error_code, long long* lhs, bool unary, char 
 		case '(':
 			// Початок підвиразу		
 			open_breket_count++;
-			_rhs = parse(&_next_operator, error_code, &_rhs, is_unary_next_operator, this_operator_priority);
+			_rhs = parse(&_next_operator, is_int_rhs, error_code, &_rhs, is_int_rhs, is_unary_next_operator, this_operator_priority);;
 			is_unary_next_operator = false;
+			is_integral_result = is_int_rhs;
 			*expr = _next_operator;
 			break;
 		case ')':
 			open_breket_count--;
 			// Кінець підвиразу.
-			if (lhs) _rhs = *lhs;
+			if (lhs) {
+				_rhs = *lhs;
+				is_int_rhs = is_int_lhs;
+			}
 			if (open_breket_count < 0) {
 				open_breket_count = 0;
-				*error_code = 5;
-				return 0;
+				error_code = 5;
+				_rhs.ll = 0;
+				return _rhs;
 			}
 			break;
 		default:
-			*error_code = 2; // Unknown binary operator.
+			error_code = 2; // Unknown binary operator.
 		}
 	} else
 		switch (this_operator)
 		{
 		case '-':
 			// Віднімання. 
-			_rhs = *lhs - expr_rhs;
+			if(is_int_rhs && is_int_lhs)
+				_rhs.ll = lhs->ll - expr_rhs.ll;
+			else {
+				_rhs.d = ((is_int_lhs)?lhs->ll:lhs->d) - ((is_int_rhs)?expr_rhs.ll:expr_rhs.d);
+				is_int_rhs = false;
+			}
 			break;
 		case '+':
 			// Додавання. 
-			_rhs = *lhs + expr_rhs;
+			if (is_int_rhs && is_int_lhs)
+				_rhs.ll = lhs->ll + expr_rhs.ll;
+			else {
+				_rhs.d = ((is_int_lhs) ? lhs->ll : lhs->d) + ((is_int_rhs) ? expr_rhs.ll : expr_rhs.d);
+				is_int_rhs = false;
+			}
 			break;
 		case '*':
 			// Множення. 
-			_rhs = *lhs * expr_rhs;
+			if (is_int_rhs && is_int_lhs)
+				_rhs.ll = lhs->ll * expr_rhs.ll;
+			else {
+				_rhs.d = ((is_int_lhs) ? lhs->ll : lhs->d) * ((is_int_rhs) ? expr_rhs.ll : expr_rhs.d);
+				is_int_rhs = false;
+			}
 			break;
 		case '/':
 			// Ділення.
-			if (!expr_rhs) {
-				*error_code = 4;
-				return 0;
+			if (is_int_rhs && is_int_lhs) {
+				if (expr_rhs.ll == 0) {
+					error_code = 4;
+					_rhs.ll = 0;
+					return _rhs;
+				}
+				if (expr_rhs.ll >= lhs->ll)
+				{
+					_rhs.d = (double)lhs->ll / expr_rhs.ll;
+					is_int_rhs = false;
+				}
+				else {
+					_rhs.ll = lhs->ll / expr_rhs.ll;
+					is_int_rhs = false;
+				}
 			}
-			_rhs = *lhs / expr_rhs;
+			else {
+				_rhs.d = ((is_int_lhs) ? lhs->ll : lhs->d ) / ((is_int_rhs) ? expr_rhs.ll : expr_rhs.d);
+				is_int_rhs = false;
+			}
 			break;
 		case '%':
 			// Залишок від ділення. 
-			_rhs = *lhs % expr_rhs;
+			if (is_int_rhs && is_int_lhs) {
+				_rhs.ll = lhs->ll % expr_rhs.ll;
+			}
+			else {
+				error_code = 8;
+				_rhs.ll = 0;
+				return _rhs;
+			}
 			break;
 		case '^':
 			// Піднесення до степеня. 
-			_rhs = std::pow(*lhs, expr_rhs);
+			lhs->d = (is_int_lhs) ? lhs->ll : lhs->d;
+			expr_rhs.d = (is_int_rhs) ? expr_rhs.ll : expr_rhs.d;
+			_rhs.d = std::pow(lhs->d, expr_rhs.d);
+			is_int_rhs = false;
 			break;
 		default:
-			*error_code = 3; // Unknown binary operator.
+			error_code = 3; // Unknown binary operator.
 		}
 	/**
 	 * Якщо пріоритет попереднього оператора менший, за пріоритет поточного, то потрібно передати попередному операторові,
@@ -242,143 +303,25 @@ long long parse(char** expr, long *error_code, long long* lhs, bool unary, char 
 	}
 	if (open_breket_count > 0 && !*_next_operator) {
 		open_breket_count = 0;
-		*error_code = 6;
-		return 0;
-	}
-	/** Продовжуємо виконання операторів */
-	if (*_next_operator)
-		_rhs = parse(&_next_operator, error_code, &_rhs, is_unary_next_operator, this_operator_priority);
-
-	*expr = _next_operator;
-	return _rhs;
-}
-
-bool strtonum(char * str, long & is_integer, long long & ll_num, double & d_num)
-{
-
-	return false;
-}
-
-double parse(char** expr, double* lhs, bool unary, char prev_operator_priority)
-{
-	char* tmp = new char[strlen(*expr) + 1];
-	strcpy(tmp, *expr);
-	// Вказівник на стоку яка містить число.
-	char* it = strtok(tmp, tokens);
-	// цей оператор - символьне позначення.
-	/*
-	 * Моживо тільки в моєму випадку, функція strtok() не замінює 0-й символ, що співпадає зодним із символів в шаблоні tokens;
-	 * пізніше  спробую перезібрати для linux, щоб переконатися в цьому.
-	 */
-	char this_operator = ((*tmp < '0' || *tmp > '9')) ? *tmp : 0;
-	// Вказівник на наступий оператор,
-	char* _next_operator = *expr + 1;
-	bool is_unary_next_operator = false;
-	if (it)
-		_next_operator = (it - tmp > 1) ? _next_operator : *expr + (it - tmp + strlen(it));
-	/**
-	 * Аналіз наступного оператора.
-	 * 1) Якщо поточний оператор - не унарний постфіксний (не в списку postfix_tokens), i наступний оператор слідує відразу за поточним
-	 * операторотом - наступий оператор буде префіксним унарним.
-	 *
-	 * 2)Якщо наступний оператор - унарний і є в списку постфіксних (postfix_tokens), і за ним слідує якийсь оператор, або кінець строки,
-	 * то наступний оператор буде унарнним постфіксним.
-	 */
-	is_unary_next_operator = !strchr(postfix_tokens, this_operator) && strchr(prefix_tokens, *(*expr + 1));
-	is_unary_next_operator = is_unary_next_operator || strchr(postfix_tokens, *_next_operator) && (strchr(tokens, *(_next_operator + 1)) || *(_next_operator + 1) == 0);
-	// пріоритет цього та наступного операторів
-	char this_operator_priority = *((unary) ? (upriority + (strchr(tokens, this_operator) - tokens)) : (priority + (strchr(tokens, this_operator) - tokens))),
-		next_operator_priority = *((is_unary_next_operator) ? (upriority + (strchr(tokens, *_next_operator) - tokens)) : (priority + (strchr(tokens, *_next_operator) - tokens)));
-
-	double expr_rhs = 0, _rhs = 0;
-	if (it)
-		_rhs = strtod(it, nullptr);
-	delete[] tmp;
-	/**
-	 * Якщо пріоритет наступного оператора вищий за пріоритет поточного оператора, то спочатку виконати наступний оператор,
-	 * а вже потім виконувати цей.
-	 */
-	if (next_operator_priority != '-' && next_operator_priority > this_operator_priority && this_operator_priority != '-') {
-		expr_rhs = _rhs;
-		while (next_operator_priority > this_operator_priority && this_operator) {
-			_rhs = parse(&_next_operator, &expr_rhs, is_unary_next_operator, this_operator_priority);
-			expr_rhs = _rhs;
-			is_unary_next_operator = strchr(postfix_tokens, *_next_operator) && (strchr(tokens, *(_next_operator + 1)) || *(_next_operator + 1) == 0);
-			next_operator_priority = *((is_unary_next_operator) ? (upriority + (strchr(tokens, *_next_operator) - tokens)) : (priority + (strchr(tokens, *_next_operator) - tokens)));
-		}
-
-	}
-	else
-		expr_rhs = _rhs;
-	// Виконання поточного оператора
-	if (unary) {
-		switch (this_operator)
-		{
-		case '-':
-			// Унарний мінус
-			_rhs = -expr_rhs;
-			break;
-			/*case '!':
-				// Факторіал лівої частини.
-				if (lhs) _rhs = factorial(*lhs);
-				break; */
-		case ')':
-			// Кінець підвиразу.
-			if (lhs) _rhs = *lhs;
-			break;
-		case '(':
-			// Початок підвиразу
-		{
-			_rhs = parse(&_next_operator, &_rhs, is_unary_next_operator, this_operator_priority);
-			is_unary_next_operator = false;
-			*expr = _next_operator;
-		}
-		}
-	}
-	else
-		switch (this_operator)
-		{
-		case '-':
-			// Віднімання. 
-			_rhs = *lhs - expr_rhs;
-			break;
-		case '+':
-			// Додавання. 
-			_rhs = *lhs + expr_rhs;
-			break;
-		case '*':
-			// Множення. 
-			_rhs = *lhs * expr_rhs;
-			break;
-		case '/':
-			// Ділення.
-			_rhs = *lhs / expr_rhs;
-			break;
-		case '%': {
-			long long ll_tmp_lhs = *lhs, ll_tmp_rhs = expr_rhs;
-			if (ll_tmp_lhs == *lhs && ll_tmp_rhs == expr_rhs) {
-				// Залишок від ділення. 
-				_rhs = ll_tmp_lhs % ll_tmp_rhs;
-			}
-			break;
-		}
-		case '^':
-			// Піднесення до степеня. 
-			_rhs = std::pow(*lhs, expr_rhs);
-			break;
-		}
-	/**
-	 * Якщо пріоритет попереднього оператора менший, за пріоритет поточного, то потрібно передати попередному операторові,
-	 * результат поточного оператора, і зберегти точку продовження в expr
-	 */
-	if (prev_operator_priority && prev_operator_priority < this_operator_priority || this_operator_priority == '-') {
-		*expr = _next_operator;
+		error_code = 6;
+		_rhs.ll = 0;
 		return _rhs;
 	}
 	/** Продовжуємо виконання операторів */
 	if (*_next_operator)
-		_rhs = parse(&_next_operator, &_rhs, is_unary_next_operator, this_operator_priority);
+		_rhs = parse(&_next_operator, is_int_rhs, error_code, &_rhs, is_int_rhs, is_unary_next_operator, this_operator_priority);
+
 	*expr = _next_operator;
+	is_integral_result = is_int_rhs;
 	return _rhs;
 }
-//#endif
+
+bool strtonum(char * str, bool & is_integer, numeric& num)
+{
+	char* NaN = nullptr;
+	num.ll = strtoll(str, &NaN, 0);
+	is_integer = str + strlen(str) == NaN;
+	if(!is_integer) num.d = strtod(str, &NaN);
+	return str + strlen(str) == NaN;
+}
+
